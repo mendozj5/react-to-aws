028e855e42fff8a706efd63d1e056de78428a274
Joel T Mendoza <joeltmendoza@Joels-Mac-mini.local>
2020-08-28 18:08:13 +0800

    Updates

---
+++ precache-manifest.6ebf3391ccf425a58fd535bc971e9af8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6181181297e5781476669ae3476f1f1",
+    "url": "/mendozj5/react-to-aws/index.html"
+  },
+  {
+    "revision": "b6cbf538aaf740db07a5",
+    "url": "/mendozj5/react-to-aws/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "daab51d0231fdaf606a3",
+    "url": "/mendozj5/react-to-aws/static/js/2.d6a59dfe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mendozj5/react-to-aws/static/js/2.d6a59dfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6cbf538aaf740db07a5",
+    "url": "/mendozj5/react-to-aws/static/js/main.32d04d6a.chunk.js"
+  },
+  {
+    "revision": "a0d951100a525e073468",
+    "url": "/mendozj5/react-to-aws/static/js/runtime-main.9271b8ea.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mendozj5/react-to-aws/static/media/logo.5d5d9eef.svg"
+  }
+]);
